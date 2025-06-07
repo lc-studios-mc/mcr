8f975aece2727b01b58eae863f1dec2736c97e03
LuckedCoronet45 <lc45@lc-studios.net>
2025-06-08 07:38:30 +0900

    update cli.ts

--- src/cli.ts
+++ src/cli.ts
@@ -1,1 +1,11 @@
 #!/usr/bin/env node
+
+import { Command } from "@commander-js/extra-typings";
+import { getPackageVersion } from "./utils.js";
+
+const program = new Command("mcr");
+
+program.description("Minecraft Bedrock Addon Compiler");
+program.version(getPackageVersion());
+
+program.parseAsync();
