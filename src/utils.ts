b91c3afce7a1f914928c8e8b0726e4ff214654bc
LuckedCoronet45 <lc45@lc-studios.net>
2025-06-09 21:46:28 +0900

    update getCurrentTimeString()

--- src/utils.ts
+++ src/utils.ts
@@ -43,17 +43,12 @@ export function getComMojangDir(beta?: boolean): string {
 export function getCurrentTimeString(): string {
 	const now = new Date();
 
-	let hours = now.getHours();
+	const hours = now.getHours().toString().padStart(2, "0");
 	const minutes = now.getMinutes().toString().padStart(2, "0");
 	const seconds = now.getSeconds().toString().padStart(2, "0");
 	const milliseconds = now.getMilliseconds().toString().padStart(3, "0");
 
-	const period = hours >= 12 ? "PM" : "AM";
-
-	hours = hours % 12;
-	hours = hours === 0 ? 12 : hours; // Convert 0 to 12 for midnight
-
-	const formattedTime = `[${hours}:${minutes}:${seconds}.${milliseconds} ${period}]`;
+	const formattedTime = `[${hours}:${minutes}:${seconds}.${milliseconds}]`;
 
 	return formattedTime;
 }
